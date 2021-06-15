76ff62b224f4740fba309ddc129ef18ab5aa78d1
X00LA <X00LA@users.noreply.github.com>
2021-06-15 03:51:17 +0200

    created fxmanifest

---
+++ radialmenu/fxmanifest.lua
@@ -0,0 +1,24 @@
+fx_version 'cerulean'
+games { 'gta5' }
+
+author 'bepo13'
+description 'FiveM Radial Menu'
+version '1.2.1'
+
+client_script {
+    "config.lua",
+    "radialmenu.lua"
+}
+
+ui_page "html/menu.html"
+
+files {
+	"html/menu.html",
+	"html/raphael.min.js",
+    "html/wheelnav.min.js",
+    "html/doors.png",
+    "html/engine.png",
+    "html/hood.png",
+    "html/key.png",
+    "html/trunk.png"
+}
